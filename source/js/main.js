a51cdc1367c7b24f5bbe8635574cc556fe9c0570
Nikita <nikitagram@gmail.com>
2019-09-26 00:48:17 +0300

    all

---
+++ source/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("#carousel-1").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        items: 1,
+        dots: true
+    });
+
+        $("#carousel-2").owlCarousel({
+        loop:true,
+        margin: 5,
+        nav:true,
+        items: 5,
+        dots: true
+    });
+
+  });
